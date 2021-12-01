9bd04d45824f5c6569603d4f095b233eb6bfca78
Bennet <totals@gmx.net>
2021-12-01 16:56:38 +0100

    Copy paste

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,36 @@
+import java.io.File
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var amount=0
+        for (k in(1..1999)){
+            if (input[k]>input[k-1]){
+                amount++
+            }
+        }
+        println("Amount of increases: $amount")
+        return amount
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var increaseAmount=0
+        val numbers= input.map { it.toInt() }
+
+        for (k in (2..1998)) {
+            val thisSum = numbers[k - 1] + numbers[k] + numbers[k + 1]
+            val lastSum = numbers[k - 2] + numbers[k - 1] + numbers[k]
+            if (thisSum > lastSum) {
+                increaseAmount++
+            }
+            val increa= thisSum > lastSum
+            //println("$lastSum < $thisSum , $increa , makes $increaseAmount, step $k")
+        }
+        println("Amount of increases: $increaseAmount")
+        return increaseAmount
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("Day01_test")
+    part1(input)
+    part2(input)
 }
