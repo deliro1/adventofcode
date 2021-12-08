0b2f57d1cfa6be0eebb0d9e80d7b28637e25cfb4
Bennet <totals@gmx.net>
2021-12-08 18:14:14 +0100

    Refactoring

--- src/Day05.kt
+++ src/Day05.kt
@@ -2,82 +2,35 @@ fun main() {
     fun part1(input: List<String>): Int {
         val inputSegmented = input.map { it.split(","," -> ") }.map{ it.map{str -> str.toInt()}}
         val maxvalue=findMaxIn2DList(inputSegmented)+1
-        val landscape = Array(maxvalue) { IntArray(maxvalue) { 0 } }
-        var dangerZones=0
+        var landscape = Array(maxvalue) { IntArray(maxvalue) { 0 } }
         var xStart: Int; var xEnd: Int; var yStart: Int; var yEnd: Int
 
         inputSegmented.forEach{
             xStart= it[0]; xEnd= it[2]; yStart= it[1]; yEnd= it[3]
-            var line=(0..0) ;var lineType=""
-            if(xStart==xEnd){
-                lineType="y"
-                line= makeRange(yStart,yEnd)
-            } else if(yStart==yEnd){
-                lineType="x"
-                line= makeRange(xStart,xEnd)
-            }
-            line.forEach{element ->
-                when(lineType){
-                    "x"-> landscape[yStart][element] +=1
-                    "y"-> landscape[element][xStart] +=1
-                }
-            }
+            landscape=horiAndVertiLines(xStart,xEnd, yStart,yEnd,landscape)
         }
-        for(row in landscape){
-            for(element in row){
-                if(element >= 2){
-                    dangerZones++}
-            }
-        }
-        return dangerZones
+        return sumDanger(landscape)
     }
-
 
     fun part2(input: List<String>): Int {
         val inputSegmented = input.map { it.split(","," -> ") }.map{ it.map{str -> str.toInt()}}
         val maxvalue=findMaxIn2DList(inputSegmented)+1
-        val landscape = Array(maxvalue) { IntArray(maxvalue) { 0 } }
-        var dangerZones=0
+        var landscape = Array(maxvalue) { IntArray(maxvalue) { 0 } }
         var xStart: Int; var xEnd: Int; var yStart: Int; var yEnd: Int
 
         inputSegmented.forEach{
             xStart= it[0]; xEnd= it[2]; yStart= it[1]; yEnd= it[3]
-            var line=(0..0) ;var lineType=""
-
-
-            if(xStart==xEnd){
-                lineType="y"
-                line= makeRange(yStart,yEnd)
-            } else if(yStart==yEnd){
-                lineType="x"
-                line= makeRange(xStart,xEnd)
-            }
+            landscape=horiAndVertiLines(xStart,xEnd, yStart,yEnd,landscape)
 
             if(xStart!=xEnd && yStart!=yEnd){
-                lineType="d"
                 val xPosis= xStart.toward(xEnd).toList()
                 val yPosis= yStart.toward(yEnd).toList()
                 for (k in 0..xPosis.lastIndex ){
                     landscape[yPosis[k]][xPosis[k]] +=1
                 }
             }
-
-
-            line.forEach{element ->
-                when(lineType){
-                    "x"-> landscape[yStart][element] +=1
-                    "y"-> landscape[element][xStart] +=1
-                }
-            }
         }
-        for(row in landscape){
-            for(element in row){
-                if(element >= 2){
-                    dangerZones++
-                }
-            }
-        }
-        return dangerZones
+        return sumDanger(landscape)
     }
 
     val testInput = readInput("Day05_test")
@@ -99,6 +52,7 @@ private fun findMaxIn2DList(inputArray: List<List<Int>>): Int{
     }
     return maxvalue
 }
+
 private fun makeRange(a: Int, b: Int): IntRange{
     return if (a < b) {
         (a..b)
@@ -109,4 +63,34 @@ private fun makeRange(a: Int, b: Int): IntRange{
 private infix fun Int.toward(to: Int): IntProgression {
     val step = if (this > to) -1 else 1
     return IntProgression.fromClosedRange(this, to, step)
+}
+private fun sumDanger(landscape: Array<IntArray>):Int{
+    var dangerZones=0
+    for(row in landscape){
+        for(element in row){
+            if(element >= 2){
+                dangerZones++
+            }
+        }
+    }
+    return dangerZones
+}
+
+private fun horiAndVertiLines(xStart: Int,xEnd: Int, yStart: Int,yEnd: Int,landscape: Array<IntArray>): Array<IntArray>{
+    var lineType=""
+    var line=(0..0)
+    if(xStart==xEnd){
+        lineType="y"
+        line= makeRange(yStart,yEnd)
+    } else if(yStart==yEnd){
+        lineType="x"
+        line= makeRange(xStart,xEnd)
+    }
+    line.forEach{element ->
+        when(lineType){
+            "x"-> landscape[yStart][element] +=1
+            "y"-> landscape[element][xStart] +=1
+        }
+    }
+    return landscape
 }
