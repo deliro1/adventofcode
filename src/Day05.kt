086b81fbf9b13301d49575ac163ab050c99b352e
Bennet <totals@gmx.net>
2021-12-08 17:54:47 +0100

    Part 2 fertig. refactoring todo

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,7 +1,7 @@
 fun main() {
     fun part1(input: List<String>): Int {
         val inputSegmented = input.map { it.split(","," -> ") }.map{ it.map{str -> str.toInt()}}
-        val maxvalue=findMaxIn2DList(inputSegmented)
+        val maxvalue=findMaxIn2DList(inputSegmented)+1
         val landscape = Array(maxvalue) { IntArray(maxvalue) { 0 } }
         var dangerZones=0
         var xStart: Int; var xEnd: Int; var yStart: Int; var yEnd: Int
@@ -16,14 +16,12 @@ fun main() {
                 lineType="x"
                 line= makeRange(xStart,xEnd)
             }
-
             line.forEach{element ->
                 when(lineType){
                     "x"-> landscape[yStart][element] +=1
                     "y"-> landscape[element][xStart] +=1
                 }
             }
-
         }
         for(row in landscape){
             for(element in row){
@@ -35,14 +33,56 @@ fun main() {
     }
 
 
-
     fun part2(input: List<String>): Int {
-        return 0
+        val inputSegmented = input.map { it.split(","," -> ") }.map{ it.map{str -> str.toInt()}}
+        val maxvalue=findMaxIn2DList(inputSegmented)+1
+        val landscape = Array(maxvalue) { IntArray(maxvalue) { 0 } }
+        var dangerZones=0
+        var xStart: Int; var xEnd: Int; var yStart: Int; var yEnd: Int
+
+        inputSegmented.forEach{
+            xStart= it[0]; xEnd= it[2]; yStart= it[1]; yEnd= it[3]
+            var line=(0..0) ;var lineType=""
+
+
+            if(xStart==xEnd){
+                lineType="y"
+                line= makeRange(yStart,yEnd)
+            } else if(yStart==yEnd){
+                lineType="x"
+                line= makeRange(xStart,xEnd)
+            }
+
+            if(xStart!=xEnd && yStart!=yEnd){
+                lineType="d"
+                val xPosis= xStart.toward(xEnd).toList()
+                val yPosis= yStart.toward(yEnd).toList()
+                for (k in 0..xPosis.lastIndex ){
+                    landscape[yPosis[k]][xPosis[k]] +=1
+                }
+            }
+
+
+            line.forEach{element ->
+                when(lineType){
+                    "x"-> landscape[yStart][element] +=1
+                    "y"-> landscape[element][xStart] +=1
+                }
+            }
+        }
+        for(row in landscape){
+            for(element in row){
+                if(element >= 2){
+                    dangerZones++
+                }
+            }
+        }
+        return dangerZones
     }
 
     val testInput = readInput("Day05_test")
-    //check(part1(testInput) == 5)
-    //check(part2(testInput) == 0)
+    check(part1(testInput) == 5)
+    check(part2(testInput) == 12)
 
     val input = readInput("Day05")
     println("Part 1 result for Day05 is: ${part1(input)}")
@@ -65,4 +105,8 @@ private fun makeRange(a: Int, b: Int): IntRange{
     }else{
         (b..a)
     }
+}
+private infix fun Int.toward(to: Int): IntProgression {
+    val step = if (this > to) -1 else 1
+    return IntProgression.fromClosedRange(this, to, step)
 }
