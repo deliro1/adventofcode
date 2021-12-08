6b0bac1df04a1e0d3982d8b7773f097812f0853f
Bennet <totals@gmx.net>
2021-12-08 18:44:55 +0100

    evn more refactoring

---
+++ src/AdventOfCode2021_MAIN.kt
@@ -0,0 +1,13 @@
+package main
+
+//import day05.Day05
+
+fun main() {
+    Day01.runDay()
+//    Day02.runDay()
+//    Day03.runDay()
+//    Day04.runDay()
+//    Day05.runDay()
+//    Day06.runDay()
+//    Day07.runDay()
+}
