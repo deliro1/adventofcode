88bbb5918422dd393a4197fbcf753513a2ba3ea8
Bennet <totals@gmx.net>
2021-12-11 16:19:21 +0100

    Day 09 part 2

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -10,7 +10,7 @@ class Day09 {
         fun runDay() {
             val testInput = readInput("${filePath}_test")
             check(part1(testInput) == 15)
-            //check(part2(testInput) == 5)
+            check(part2(testInput) == 1134)
 
             val input = readInput(filePath)
             println("Part 1 result for $day is: ${part1(input)}")
@@ -20,7 +20,7 @@ class Day09 {
         private fun part1(input: List<String>): Int {
 
             var dangerLevel=0
-            var inputInt= input.map { it.map { f-> f.digitToInt() } }
+            val inputInt= input.map { it.map { f-> f.digitToInt() } }
 
 
             inputInt.forEachIndexed { y, line ->
@@ -34,12 +34,44 @@ class Day09 {
             return dangerLevel
         }
 
+
         private fun part2(input: List<String>): Int {
-            return 0
+
+            val inputInt= input.map { it.map { f-> f.digitToInt() }.toMutableList() }
+            var basinMap =inputInt
+            var basinSizes= mutableListOf<Int>()
+
+            inputInt.forEachIndexed { y, line ->
+                line.forEachIndexed { x, value ->
+                    if (value <=8) basinMap[y][x]=0
+                }
+            }
+
+            basinMap.forEachIndexed { y, line ->
+                line.forEachIndexed { x, value ->
+                    var basincount= mutableListOf<Int>(0)
+                    checkForBasin(x,y,basincount,line,basinMap)
+                    if(basincount[0]!=0){
+                        basinSizes.add(basincount[0])
+                    }
+                }
+            }
+            return basinSizes.sorted().takeLast(3).reduce { acc, i ->  acc*i}
         }
 
-        private fun testF(inputArray: List<List<Int>>): Int{
-            return 0
+        fun checkForBasin(x:Int,y:Int,basincount:MutableList<Int>,line:MutableList<Int>,basinMap:List<MutableList<Int>>){
+            if (basinMap[y][x]==9) return
+            else if (basinMap[y][x]==1) return
+            else if (basinMap[y][x]==0) {
+                basincount[0]++
+                basinMap[y][x]=1
+
+                if(x>0) checkForBasin(x-1,y,basincount,line,basinMap)
+                if(y>0) checkForBasin(x,y-1,basincount,line,basinMap)
+                if(x<line.size-1) checkForBasin(x+1,y,basincount,line,basinMap)
+                if(y<basinMap.size-1) checkForBasin(x,y+1,basincount,line,basinMap)
+            }
+
         }
 
     }
