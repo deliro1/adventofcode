b321d1bf63a089280638afccc1b0d9d90e2e2b7d
Bennet <totals@gmx.net>
2021-12-18 21:20:08 +0100

    Day 08 cleaning up

--- src/day08/Day08.kt
+++ src/day08/Day08.kt
@@ -37,7 +37,7 @@ class Day08 {
         fun part2(input: List<String>): Int {
             val digitsTemplate = input.map { it.split(" | ") }
             var count =0
-            var cypher = mutableListOf<String>("","","","","","","","","","")
+            var cypher = Array<String>(10){""}
 
             digitsTemplate.forEach {
                 val digitsStr = it[0].split(" ").map() {k-> String(k.toCharArray().apply { sort() }) }.sortedBy { itt-> itt.length }
@@ -45,41 +45,37 @@ class Day08 {
                 cypher[7]=digitsStr[1]
                 cypher[4]=digitsStr[2]
                 cypher[8]=digitsStr[9]
-                var cypherL=cypher.toMutableList()
+
 
                 digitsStr.forEach{str ->
                     var strList= str.toList()
-                    if(str.length==5 && strList.containsAll(cypherL[1].toList())) cypherL[3]=str
-                    else if(str.length==5 && strList.containsAll(cypherL[4].toList().minus(cypherL[1].toList()))) cypherL[5]=str
-                    else if(str.length==5 && strList.containsAll(cypherL[8].toList().minus(cypherL[4].toList()))) cypherL[2]=str
-
-                    else if(str.length==6 && strList.containsAll(cypherL[8].toList().minus(cypherL[1].toList()))) cypherL[6]=str
-                    else if(str.length==6 && strList.containsAll(cypherL[8].toList().minus(cypherL[5].toList()))) cypherL[0]=str
-                    else if(str.length==6) cypherL[9]=str
+                    if(str.length==5 && strList.containsAll(cypher[1].toList())) cypher[3]=str
+                    else if(str.length==5 && strList.containsAll(cypher[4].toList().minus(cypher[1].toList()))) cypher[5]=str
+                    else if(str.length==5 && strList.containsAll(cypher[8].toList().minus(cypher[4].toList()))) cypher[2]=str
+                    else if(str.length==6 && strList.containsAll(cypher[8].toList().minus(cypher[1].toList()))) cypher[6]=str
+                    else if(str.length==6 && strList.containsAll(cypher[8].toList().minus(cypher[5].toList()))) cypher[0]=str
+                    else if(str.length==6) cypher[9]=str
                 }
                 val digitsCode = it[1].split(" ").map() {k-> String(k.toCharArray().apply { sort() }) }
 
                 var nochnstring=""
                 digitsCode.forEach{code ->
                     when(code){
-                        cypherL[0] -> nochnstring+="0"
-                        cypherL[1] -> nochnstring+="1"
-                        cypherL[2] -> nochnstring+="2"
-                        cypherL[3] -> nochnstring+="3"
-                        cypherL[4] -> nochnstring+="4"
-                        cypherL[5] -> nochnstring+="5"
-                        cypherL[6] -> nochnstring+="6"
-                        cypherL[7] -> nochnstring+="7"
-                        cypherL[8] -> nochnstring+="8"
-                        cypherL[9] -> nochnstring+="9"
+                        cypher[0] -> nochnstring+="0"
+                        cypher[1] -> nochnstring+="1"
+                        cypher[2] -> nochnstring+="2"
+                        cypher[3] -> nochnstring+="3"
+                        cypher[4] -> nochnstring+="4"
+                        cypher[5] -> nochnstring+="5"
+                        cypher[6] -> nochnstring+="6"
+                        cypher[7] -> nochnstring+="7"
+                        cypher[8] -> nochnstring+="8"
+                        cypher[9] -> nochnstring+="9"
                     }
                 }
                 count += nochnstring.toInt()
-
             }
             return count
         }
-
-
     }
 }
