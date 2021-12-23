04b938e8e776db17b1110efcd54d3a99dd1aecbf
Bennet <totals@gmx.net>
2021-12-23 17:49:56 +0100

    Day 10 part 2

---
+++ src/day12/Day12.kt
@@ -0,0 +1,35 @@
+package day12
+import readInput
+
+class Day12 {
+    companion object {
+
+        private const val day = "day12"
+        private val filePath = "$day/${day.capitalize()}"
+
+        fun runDay() {
+            val testInput = readInput("${filePath}_test")
+            check(part1(testInput) == 10)
+            //check(part2(testInput) == 5)
+
+            val input = readInput(filePath)
+            println("Part 1 result for $day is: ${part1(input)}")
+            println("Part 2 result for $day is: ${part2(input)}")
+        }
+
+        fun part1(input: List<String>): Int {
+            println("inout")
+
+
+
+            return 10
+        }
+
+        fun part2(input: List<String>): Int {
+            return 0
+        }
+
+
+    }
+}
+
